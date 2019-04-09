4f0d9684caec755d76365630dd1f6c5f367bc124
SencerH <sencerhamarat@gmail.com>
2019-04-09 15:29:01 +0300

    Initial Commit

--- api/views.py
+++ api/views.py
@@ -1,30 +1,17 @@
 # coding: utf-8
-import logging
+import json
 
+import jwt
+from django.http import HttpResponse
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, views, exceptions
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
 from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.response import Response
 
 from api.models import User, Customer
 from api.permissions import IsLoggedInUserOrAdmin
-from api.serializers import UserSerializer, CustomerSerializer
-
-log = logging.getLogger(__name__)
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def get_permissions(self):
-        permission_classes = []
-        if self.action == 'create':
-            permission_classes = [AllowAny]
-        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
-            permission_classes = [IsLoggedInUserOrAdmin]
-        elif self.action == 'list' or self.action == 'destroy':
-            permission_classes = [IsAdminUser]
-        return [permission() for permission in permission_classes]
+from api.serializers import CustomerSerializer
 
 
 def start_page(request, title="Simple Django RESTFull App"):
@@ -46,3 +33,92 @@ class CustomerViewSet(viewsets.ModelViewSet):
         elif self.action == 'list' or self.action == 'destroy':
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
+
+
+class Login(views.APIView):
+
+    def post(self, request, *args, **kwargs):
+        if not request.data:
+            return Response({'Error': "Please provide username/password"}, status="400")
+
+        username = request.data['username']
+        password = request.data['password']
+        try:
+            user = User.objects.get(username=username, password=password)
+        except User.DoesNotExist:
+            return Response({'Error': "Invalid username/password"}, status="400")
+        if user:
+            payload = {
+                'id': user.id,
+                'email': user.email,
+            }
+            jwt_token = {'token': jwt.encode(payload, "SECRET_KEY")}
+
+            return HttpResponse(
+                json.dumps(jwt_token),
+                status=200,
+                content_type="application/json"
+            )
+        else:
+            return Response(
+                json.dumps({'Error': "Invalid credentials"}),
+                status=400,
+                content_type="application/json"
+            )
+
+
+class TokenAuthentication(BaseAuthentication):
+    model = None
+
+    def get_model(self):
+        return User
+
+    def authenticate(self, request):
+        auth = get_authorization_header(request).split()
+        if not auth or auth[0].lower() != b'token':
+            return None
+
+        if len(auth) == 1:
+            msg = 'Invalid token header. No credentials provided.'
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = 'Invalid token header'
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            token = auth[1]
+            if token == "null":
+                msg = 'Null token not allowed'
+                raise exceptions.AuthenticationFailed(msg)
+        except UnicodeError:
+            msg = 'Invalid token header. Token string should not contain invalid characters.'
+            raise exceptions.AuthenticationFailed(msg)
+
+        return self.authenticate_credentials(token)
+
+    def authenticate_credentials(self, token):
+        # model = self.get_model()
+        payload = jwt.decode(token, "SECRET_KEY")
+        email = payload['email']
+        userid = payload['id']
+        msg = {'Error': "Token mismatch", 'status': "401"}
+        try:
+
+            user = User.objects.get(
+                email=email,
+                id=userid,
+                is_active=True
+            )
+
+            if not user.token['token'] == token:
+                raise exceptions.AuthenticationFailed(msg)
+
+        except jwt.ExpiredSignature or jwt.DecodeError or jwt.InvalidTokenError:
+            return HttpResponse({'Error': "Token is invalid"}, status="403")
+        except User.DoesNotExist:
+            return HttpResponse({'Error': "Internal server error"}, status="500")
+
+        return user, token
+
+    def authenticate_header(self, request):
+        return 'Token'
