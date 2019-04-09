07f18eb991166c2b32413e869417c9950d2364bd
SencerH <sencerhamarat@gmail.com>
2019-04-09 12:30:06 +0300

    Initial Commit

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,73 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User, UserProfile, CustomerAddress, CustomerPhone, CustomerMail, CustomerWebSite, Customer
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    inlines = (UserProfileInline, )
+
+
+class AddressInline(admin.TabularInline):
+    pass
+
+
+class CustomerAddressInline(AddressInline):
+    model = CustomerAddress
+    extra = 1
+
+
+class PhoneInline(admin.TabularInline):
+    pass
+
+
+class CustomerPhoneInline(PhoneInline):
+    model = CustomerPhone
+    extra = 1
+
+
+class EmailInline(admin.TabularInline):
+    pass
+
+
+class CustomerEmailInline(EmailInline):
+    model = CustomerMail
+    extra = 1
+
+
+class WebSiteInline(admin.TabularInline):
+    pass
+
+
+class CustomerWebSiteInline(WebSiteInline):
+    model = CustomerWebSite
+    extra = 1
+
+
+@register(Customer)
+class CompanyAdmin(admin.ModelAdmin):
+    inlines = (CustomerAddressInline, CustomerPhoneInline, CustomerEmailInline, CustomerWebSiteInline)
